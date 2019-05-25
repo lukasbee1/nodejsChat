883cafa07f1ba09b01642ac1aa03bdd565585e39
Maksim <lukashevich2014@gmail.com>
2019-05-25 15:28:35 +0300

    lint fixing

--- src/Chat.js
+++ src/Chat.js
@@ -1,32 +1,37 @@
+/* eslint-disable no-console */
 class Chat {
   constructor(name) {
     this.chatName = name;
     this.users = [];
     this.state = false;
-    this.id = `f${(+new Date).toString(16)}`;
+    this.id = `f${(+new Date()).toString(16)}`;
   }
+
   addUser(user) {
     this.users.push(user);
-    console.log('user with ID: ' + user.remotePort + ' added');
+    console.log(`user with ID: ${user.remotePort} added`);
   }
+
   removeUser(user) {
     const index = this.users.indexOf(user);
     this.users.splice(index, 1);
   }
+
   setState(state) {
     this.state = state;
   }
-  inChat = (user) => {
-    if (this.users.includes(user))
-      return true;
-    else
-      return false;
+
+  inChat(user) {
+    if (this.users.includes(user)) return true;
+    return false;
   }
+
   getID() {
     return this.id;
   }
+
   static getChatByID(id, chatList) {
     return chatList.filter(chat => chat.id.toString() === id.trim())[0];
   }
 }
-module.exports = Chat; 
+module.exports = Chat;
--- src/nodeEntry.js
+++ src/nodeEntry.js
@@ -1,22 +1,20 @@
-//Load HTTP module
-const http = require("http");
+// Load HTTP module
+const http = require('http');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//Create HTTP server and listen on port 3000 for requests
+// Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
+  // Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   if (req.url === '/') {
     res.statusCode = 302;
     res.setHeader('Location', '/entry');
-  } 
-  else if (req.url === '/entry') {
+  } else if (req.url === '/entry') {
     res.write('home');
-  }
-  else if (req.url === '/hello') {
+  } else if (req.url === '/hello') {
     res.write('world');
   } else {
     res.write('404: not found');
@@ -25,7 +23,7 @@ const server = http.createServer((req, res) => {
   res.end();
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
+// listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
